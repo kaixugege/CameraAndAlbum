fa7e10713add20a9c921b0cd61877e966e8ae253
kaixugege <604312074@qq.com>
2019-06-17 14:37:42 +0800

    添加了一些打开相册的方法，

--- app/src/main/java/com/xugege/cameraandalbum/ContentActivity.java
+++ app/src/main/java/com/xugege/cameraandalbum/ContentActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Message;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,19 +14,31 @@ import android.widget.Toast;
 
 public class ContentActivity extends AppCompatActivity {
     public final int REQUEST_CODE_GET_PIC_URI = 0X12;
-
+    private static final int CHOOSE_PICTURE = 4;
+    private static final int PICK_PICTURE_REQUESTCODE = 111;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
 
     }
-
+    private void pickPicture() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setDataAndType(MediaStore.Images.Media.INTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intent, PICK_PICTURE_REQUESTCODE);
+    }
     public void openAlbum(View view) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, REQUEST_CODE_GET_PIC_URI);
+//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//        intent = new Intent(Intent.ACTION_PICK);
+//        intent.setType("image/*");
+//        startActivityForResult(intent, REQUEST_CODE_GET_PIC_URI);
+
+
+//        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+//        startActivityForResult(intent, CHOOSE_PICTURE);
+        pickPicture();
+
     }
 
     public void openCarmera(View view) {
@@ -40,8 +53,18 @@ public class ContentActivity extends AppCompatActivity {
             switch (requestCode) {
                 case REQUEST_CODE_GET_PIC_URI:
                     Uri uri = data.getData();
-                    Log.e(this.getClass().getSimpleName(), "onActivityResult: uri:" + uri.toString());
-                    Toast.makeText(this, "" + uri.toString(), Toast.LENGTH_LONG).show();
+                    Log.e(this.getClass().getSimpleName(), "REQUEST_CODE_GET_PIC_URI  onActivityResult: uri:" + uri.toString());
+                    Toast.makeText(this, "REQUEST_CODE_GET_PIC_URI  " + uri.toString(), Toast.LENGTH_LONG).show();
+                    break;
+                case CHOOSE_PICTURE:
+                    Uri uris = data.getData();
+                    Log.e(this.getClass().getSimpleName(), "CHOOSE_PICTURE  onActivityResult: uri:" + uris.toString());
+                    Toast.makeText(this, "CHOOSE_PICTURE  " + uris.toString(), Toast.LENGTH_LONG).show();
+                    break;
+                case PICK_PICTURE_REQUESTCODE:
+                    Uri uriss = data.getData();
+                    Log.e(this.getClass().getSimpleName(), "PICK_PICTURE_REQUESTCODE  onActivityResult: uri:" + uriss.toString());
+                    Toast.makeText(this, "PICK_PICTURE_REQUESTCODE  " + uriss.toString(), Toast.LENGTH_LONG).show();
                     break;
             }
         }
